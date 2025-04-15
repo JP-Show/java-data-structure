e6af58ada3df835567ff94504439d5be38cd88a1
Drezzy <andreluiz.jpofi@gmail.com>
2025-04-15 15:15:03 -0300

    lesson02: improve adding method, added size atribute.

--- vetores/src/Vector.java
+++ vetores/src/Vector.java
@@ -2,8 +2,17 @@ package vetores.src;
 
 public class Vector {
     private String[] elements;
+    private int size;
 
     public Vector(int size){
         this.elements = new String[size];
+        this.size = 0;
+    }
+    
+    public void add(String element){
+        if(this.size < this.elements.length){
+            this.elements[this.size++] = element;
+        }else 
+            throw new RuntimeException("Vector is full, can't add more elements");
     }
 }
---
+++ vetores/src/teste/Lesson02.java
@@ -0,0 +1,13 @@
+package vetores.src.teste;
+
+import vetores.src.Vector;
+
+public class Lesson02 {
+    
+    public static void main(String[] args) {
+        Vector vector = new Vector(2);
+
+        vector.add("Hello");
+        vector.add("World");
+    }
+}
