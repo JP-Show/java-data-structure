fe92bf28469f73dd17342d289b0137e5ae16f864
Drezzy <andreluiz.jpofi@gmail.com>
2025-04-15 14:47:57 -0300

    leasson 1 - creating a vector class

---
+++ vetores/src/Vector.java
@@ -0,0 +1,9 @@
+package vetores.src;
+
+public class Vector {
+    private String[] elements;
+
+    public Vector(int size){
+        this.elements = new String[size];
+    }
+}
---
+++ vetores/src/teste/Test.java
@@ -0,0 +1,7 @@
+package vetores.src.teste;
+
+import vetores.src.Vector;;
+
+public class Test {
+    Vector vector = new Vector(5);
+}
